b53fcffa3e77d8c2d5f74b2f7507a4c28963e172
JinYangHo <qkfkadlemf@naver.com>
2021-10-16 16:53:22 +0900

    mainpage 템플릿2 적용

--- project/project/urls.py
+++ project/project/urls.py
@@ -22,6 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('mainpage/', include('mainpage.urls')),
+    path('stock/', include('stock.urls')),
 ]
 
 #파일 업로드를 위한 설정.
